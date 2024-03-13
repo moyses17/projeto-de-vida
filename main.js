84e94bd4f66f5cb5436636e8098a46a0b30fdea6
norninha do grau <106700570+moyses17@users.noreply.github.com>
2024-03-13 14:34:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -19,3 +19,11 @@ textos[j].classlist.remove("ativo")
 
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+  let temporeal = new Date();
+  let tempofinal = tempoObjetivo - tempo Atual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60 );
+  let horas = Math.floor(minutos / 60 );
+  let dias = Math.floor(horas / 24);
