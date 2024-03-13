fc28216812d78a30c4ced53eefa8d689644adbee
norninha do grau <106700570+moyses17@users.noreply.github.com>
2024-03-13 14:40:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,36 @@
-const botoes= document .querySelectorAll(".botao);
-const textos= document .querySelectorAll(".aba-conteudo);
-                                         for(let i=0;i <botes.length;i++){
-botoes[i].onclick = function(){
+const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".aba-conteudo");
 
-  for(let j=0;j<botoes.length;++){
-    botoes[j].classlist.remove("ativo);
-                               }
-textos[j].classlist.remove("ativo")
-  botoes[i].vlaList.add("ativo")
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+    }
 }
-}
 
 
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 
+contadores[0].textContent = calculaTempo9tempoObjetivo1);
 
-
-
-
-
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo) {
-  let temporeal = new Date();
-  let tempofinal = tempoObjetivo - tempo Atual;
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
   let segundos = Math.floor(tempoFinal / 1000);
-  let minutos = Math.floor(segundos / 60 );
-  let horas = Math.floor(minutos / 60 );
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
   let dias = Math.floor(horas / 24);
+  
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+ 
+ return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}
