08d62e9d5b98f90c2296ba050fc33643faf28421
norninha do grau <106700570+moyses17@users.noreply.github.com>
2024-03-13 15:30:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -32,13 +32,14 @@ function calculaTempo(tempoObjetivo) {
   let minutos = Math.floor(segundos / 60);
   let horas = Math.floor(minutos / 60);
   let dias = Math.floor(horas / 24);
-  
-    segundos %= 60;
+   segundos %= 60;
     minutos %= 60;
     horas %= 24;
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}
+
 } else {
     return "Prazo Finalizado";
 }
 }
- return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-}
+ 
